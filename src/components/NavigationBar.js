a692cd6f631679cda25d0d4500c0f5a3b699a543
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-05 03:02:03 +0800

    modified:   src/components/NavigationBar.js 	modified:   src/pages/HomePage.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,53 +5,19 @@ import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
     return (
-        <div>
-            <div>
-            <Navbar className="sticky-nav" style={{ backgroundColor: 'rgba(0, 100, 0, 0.8)' }} expand="lg" sticky="top">
-                <Navbar.Brand href="/"><div className='logo'>TerraBorus</div></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
-                    <Nav className="nav_bar mr-auto text-white">
-                        <Nav.Link className="nav_link_custom" href="/">Home</Nav.Link>
-                        <Nav.Link className="nav_link_custom" href="/blog">Blog</Nav.Link>
-                        <Nav.Link className="nav_link_custom" href="/">About</Nav.Link>
-                        <Nav.Link className="nav_link_custom" href="/">Contact</Nav.Link>
-                        <Nav.Link className="button" href="/">Add Tree</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            </div>
-            <div>
-            <Navbar className="sticky-nav" expand="lg" sticky="top">
-                <Navbar.Brand href="/"><div className='overview'>Map Overview</div></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
-                    <Nav className="nav_bar mr-auto">
-                        <NavDropdown title="Trees Species" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/">1</NavDropdown.Item>
-                            <NavDropdown.Item href="/">2</NavDropdown.Item>
-                            <NavDropdown.Item href="/">3</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Trees List" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/">1</NavDropdown.Item>
-                            <NavDropdown.Item href="/">2</NavDropdown.Item>
-                            <NavDropdown.Item href="/">3</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Trees Status" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/">1</NavDropdown.Item>
-                            <NavDropdown.Item href="/">2</NavDropdown.Item>
-                            <NavDropdown.Item href="/">3</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Trees Cycle" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/">1</NavDropdown.Item>
-                            <NavDropdown.Item href="/">2</NavDropdown.Item>
-                            <NavDropdown.Item href="/">3</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            </div>
-        </div>
+        <Navbar className="sticky-nav" style={{ backgroundColor: 'rgba(0, 100, 0, 0.8)' }} expand="lg" sticky="top">
+            <Navbar.Brand href="/"><div className='logo'>TerraBorus</div></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
+                <Nav className="nav_bar mr-auto text-white">
+                    <Nav.Link className="nav_link_custom" href="/">Home</Nav.Link>
+                    <Nav.Link className="nav_link_custom" href="/blog">Blog</Nav.Link>
+                    <Nav.Link className="nav_link_custom" href="/">About</Nav.Link>
+                    <Nav.Link className="nav_link_custom" href="/">Contact</Nav.Link>
+                    <Nav.Link className="button" href="/">Add Tree</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar';
 import Map from '../components/Map';
+import '../styles/navbar.css'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 // import TreeList from '../components/TreeList';
 // import FAQ from '../components/FAQ';
 // import Footer from '../components/Footer';
@@ -10,6 +12,34 @@ const HomePage = () => {
     return (
         <>
             <NavigationBar />
+            <Navbar className="nav" expand="lg">
+                <Navbar.Brand href="/"><div className='overview'>Map Overview</div></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
+                    <Nav className="nav_bar mr-auto">
+                        <NavDropdown title="Trees Species" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/">1</NavDropdown.Item>
+                            <NavDropdown.Item href="/">2</NavDropdown.Item>
+                            <NavDropdown.Item href="/">3</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Trees List" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/">1</NavDropdown.Item>
+                            <NavDropdown.Item href="/">2</NavDropdown.Item>
+                            <NavDropdown.Item href="/">3</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Trees Status" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/">1</NavDropdown.Item>
+                            <NavDropdown.Item href="/">2</NavDropdown.Item>
+                            <NavDropdown.Item href="/">3</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Trees Cycle" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/">1</NavDropdown.Item>
+                            <NavDropdown.Item href="/">2</NavDropdown.Item>
+                            <NavDropdown.Item href="/">3</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             <div className='mapContainer'>
                 <Map />
             </div>
