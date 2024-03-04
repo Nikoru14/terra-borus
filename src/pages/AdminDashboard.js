41638b189295a6fa11ab53ffa167a3a4a2a327e3
Nikoru14 <nicholosantosbongat@gmail.com>
2024-03-05 02:05:46 +0800

    modified:   public/index.css 	modified:   src/pages/AdminDashboard.js 	modified:   src/pages/NotFound.js 	modified:   src/styles/tree.css

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -12,7 +12,6 @@ const AdminDashboard = () => {
     return (
         <>
             <NavigationBar />
-
         </>
     )
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,10 +5,12 @@ import '../styles/navbar.css'
 
 const NotFound = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1><i className="fa-duotone fa-triangle-exclamation"></i> 404 - Page Not Found</h1>
-            <p className="NotFound">The page you are looking for does not exist or has been moved.</p>
-            <Link to="/">Go Home</Link>
+        <div className='card'>
+            <div style={{ textAlign: 'center' }}>
+                <h1><i className="fa-duotone fa-triangle-exclamation"></i> 404 - Page Not Found</h1>
+                <p className="NotFound">The page you are looking for does not exist or has been moved.</p>
+                <Link to="/">Go Home</Link>
+            </div>
         </div>
     );
 };
