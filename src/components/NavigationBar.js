ba0c5a818afef59e359a2a02c2337481ada2d70b
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-04 10:54:48 +0800

    modified:   src/components/NavigationBar.js 	modified:   src/styles/navbar.css

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,7 +7,7 @@ const NavigationBar = () => {
         <Navbar bg="dark" expand="lg" data-bs-theme="dark">
             <Navbar.Brand href="/"><div className='logo'>TerraBorus</div></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
+            <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
                 <Nav className="nav_bar mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/">Blog</Nav.Link>
