b4b680d3ad6a6fe512cb66003639a5dea99fa6a8
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-05 02:43:28 +0800

    modified:   src/components/NavigationBar.js 	renamed:    public/img/bghome.png -> src/images/bghome.png 	modified:   src/styles/navbar.css

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,11 +10,11 @@ const NavigationBar = () => {
                 <Navbar.Brand href="/"><div className='logo'>TerraBorus</div></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
-                    <Nav className="nav_bar mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/blog">Blog</Nav.Link>
-                        <Nav.Link href="/">About</Nav.Link>
-                        <Nav.Link href="/">Contact</Nav.Link>
+                    <Nav className="nav_bar mr-auto text-white">
+                        <Nav.Link className="nav_link_custom" href="/">Home</Nav.Link>
+                        <Nav.Link className="nav_link_custom" href="/blog">Blog</Nav.Link>
+                        <Nav.Link className="nav_link_custom" href="/">About</Nav.Link>
+                        <Nav.Link className="nav_link_custom" href="/">Contact</Nav.Link>
                         <Nav.Link className="button" href="/">Add Tree</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
