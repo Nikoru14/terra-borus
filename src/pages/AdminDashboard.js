088b58cdabf4b2f845e1a8581bce767f0065c20d
Nikoru14 <nicholosantosbongat@gmail.com>
2024-03-05 03:49:43 +0800

    modified:   src/pages/AdminDashboard.js 	modified:   src/pages/TreeInfo.js

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import NavigationBar from "../components/NavigationBar";
 import QRCode from 'react-qr-code';
 import treeData from '../json/TreeData.json'; // Adjust the import path as needed
@@ -10,11 +10,42 @@ const AdminDashboard = () => {
     }));
 
     const [selectedTreeForQR, setSelectedTreeForQR] = useState(null);
-
+    const qrRef = useRef(null);
     // Function to generate the QR code URL dynamically
     const handleQRCodeGenerate = (treeId) => {
         const qrValue = `${window.location.origin}/TreeInfo?treeId=${treeId}`;
         setSelectedTreeForQR(qrValue);
+    };
+
+    const downloadQRCode = () => {
+        const svg = qrRef.current.querySelector('svg');
+        const svgData = new XMLSerializer().serializeToString(svg);
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const img = new Image();
+        const svgSize = svg.getBoundingClientRect();
+        const borderSize = 10;
+
+        canvas.width = svgSize.width + borderSize * 2;
+        canvas.height = svgSize.height + borderSize * 2;
+
+        img.onload = () => {
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.drawImage(img, borderSize, borderSize, svgSize.width, svgSize.height);
+
+            const pngFile = canvas.toDataURL("image/png");
+
+            const downloadLink = document.createElement('a');
+            downloadLink.href = pngFile;
+            downloadLink.download = 'QRCode.png';
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        };
+
+        img.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgData)));
     };
 
     return (
@@ -48,10 +79,10 @@ const AdminDashboard = () => {
                 </table>
                 {selectedTreeForQR && (
                     <div style={{ marginTop: '20px', textAlign: 'center', padding: '16px', background: 'white' }}>
-                        {/* Display the QR Code */}
-                        <QRCode value={selectedTreeForQR} />
-                        {/* Optionally, display the URL below the QR code for reference */}
-                        <p>QR Code for Tree ID: {selectedTreeForQR.split('=')[1]}</p>
+                        <div ref={qrRef}>
+                            <QRCode value={selectedTreeForQR} />
+                        </div>
+                        <button onClick={downloadQRCode}>Save as PNG</button>
                     </div>
                 )}
             </div>
