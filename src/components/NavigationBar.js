313882a3d6dcd0e164edd405bdd0f9eb70c5fb01
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-04 09:26:53 +0800

    modified:   src/components/NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,11 +3,11 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const NavigationBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" expand="lg" data-bs-theme="dark">
             <Navbar.Brand href="/"><div className='logo'>TerraBorus</div></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="nav_bar  mr-auto">
+            <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
+                <Nav className="nav_bar mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/">Blog</Nav.Link>
                     <Nav.Link href="/">About</Nav.Link>
