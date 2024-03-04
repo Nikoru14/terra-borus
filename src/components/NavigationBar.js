f76c6a57176418a0279ee959f32c2c6e72e56f77
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-04 09:10:48 +0800

    modified:   src/components/NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,8 +7,12 @@ const NavigationBar = () => {
             <Navbar.Brand href="/"><div className='logo'>TerraBorus</div></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
+                <Nav className="nav_bar  mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/">Blog</Nav.Link>
+                    <Nav.Link href="/">About</Nav.Link>
+                    <Nav.Link href="/">Contact</Nav.Link>
+                    <Nav.Link href="/">Add Tree</Nav.Link>
                     {/* <Nav.Link href="/blog">Blog</Nav.Link>
                     ... other links */}
                 </Nav>
