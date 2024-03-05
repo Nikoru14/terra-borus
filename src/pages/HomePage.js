f5cbf2f512c9f227d845ff72d5ef4157c2cde9ad
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-05 13:16:48 +0800

    modified:   public/index.css 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/images/bg2.png 	modified:   src/pages/HomePage.js 	modified:   src/styles/navbar.css 	modified:   src/styles/tree.css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,9 @@ import NavigationBar from '../components/NavigationBar';
 import Map from '../components/Map';
 import '../styles/navbar.css'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 // import TreeList from '../components/TreeList';
 // import FAQ from '../components/FAQ';
 // import Footer from '../components/Footer';
@@ -11,12 +14,12 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 const HomePage = () => {
     return (
         <>
+        <div>
+            <div className='col m-12'></div>
             <NavigationBar />
             <Navbar className="nav" expand="lg">
                 <Navbar.Brand href="/"><div className='overview'>Map Overview</div></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="nav-collapse justify-content-center" id="basic-navbar-nav">
-                    <Nav className="nav_bar mr-auto">
+                    <Nav className="nav_bar1 mr-auto">
                         <NavDropdown title="Trees Species" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/">1</NavDropdown.Item>
                             <NavDropdown.Item href="/">2</NavDropdown.Item>
@@ -38,11 +41,12 @@ const HomePage = () => {
                             <NavDropdown.Item href="/">3</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                </Navbar.Collapse>
             </Navbar>
             <div className='mapContainer'>
                 <Map />
             </div>
+            </div>
+
             {/*<TreeList />
             <FAQ />
             <Footer />*/}
