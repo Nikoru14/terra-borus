e912b40b8764e5fac284c8c1c6f913f0045cc323
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-08 23:32:47 +0800

    modified:   public/index.css 	modified:   src/components/Map.js 	modified:   src/styles/login-register.css 	modified:   src/styles/navbar.css 	modified:   src/styles/tree.css

--- src/components/Map.js
+++ src/components/Map.js
@@ -102,36 +102,32 @@ const Map = () => {
 };
 
 const MapNavBar = () => {
-
     return (
-        <Navbar className="nav" expand="lg">
+        <div className="nav" expand="lg">
             <Navbar.Brand href="/"><div className='overview'>Map Overview</div></Navbar.Brand>
-            <Nav className="nav_bar1 d-flex justify-content-end">
+            <Nav className="nav_bar1 justify-content-end">
                 <NavDropdown title="Trees Species" id="species-dropdown" className="species">
                     <NavDropdown.Item href="/">1</NavDropdown.Item>
                     <NavDropdown.Item href="/">2</NavDropdown.Item>
                     <NavDropdown.Item href="/">3</NavDropdown.Item>
                 </NavDropdown>
-
                 <NavDropdown title="Trees List" id="list-dropdown" className="list">
                     <NavDropdown.Item href="/">1</NavDropdown.Item>
                     <NavDropdown.Item href="/">2</NavDropdown.Item>
                     <NavDropdown.Item href="/">3</NavDropdown.Item>
                 </NavDropdown>
-
                 <NavDropdown title="Trees Status" id="status-dropdown" className="status">
                     <NavDropdown.Item href="/">1</NavDropdown.Item>
                     <NavDropdown.Item href="/">2</NavDropdown.Item>
                     <NavDropdown.Item href="/">3</NavDropdown.Item>
                 </NavDropdown>
-
                 <NavDropdown title="Trees Cycle" id="cycle-dropdown" className="cycle">
                     <NavDropdown.Item href="/">1</NavDropdown.Item>
                     <NavDropdown.Item href="/">2</NavDropdown.Item>
                     <NavDropdown.Item href="/">3</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
-        </Navbar>
+        </div>
     );
 }
 
