02b10a61706affd6d2e68faeb32a692088739757
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-09 23:26:26 +0800

    modified:   src/components/Map.js 	modified:   src/styles/navbar.css

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import ReactMapGL, { NavigationControl, Marker, Popup } from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 import { debounce } from "lodash";
@@ -108,21 +108,21 @@ const MapNavBar = () => {
             <Navbar.Brand href="/"><div className='overview'>Map Overview</div></Navbar.Brand>
             <Nav className="nav_bar1 d-flex justify-content-end">
                 <NavDropdown title="Trees Species" id="species-dropdown" className="species">
-                    <NavDropdown.Item href="/">1</NavDropdown.Item>
-                    <NavDropdown.Item href="/">2</NavDropdown.Item>
-                    <NavDropdown.Item href="/">3</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">1</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">2</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">3</NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Trees List" id="list-dropdown" className="list">
-                    <NavDropdown.Item href="/">1</NavDropdown.Item>
-                    <NavDropdown.Item href="/">2</NavDropdown.Item>
-                    <NavDropdown.Item href="/">3</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">1</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">2</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">3</NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Trees Status" id="status-dropdown" className="status">
-                    <NavDropdown.Item href="/">1</NavDropdown.Item>
-                    <NavDropdown.Item href="/">2</NavDropdown.Item>
-                    <NavDropdown.Item href="/">3</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">1</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">2</NavDropdown.Item>
+                    <NavDropdown.Item href="/" className="nav-link-custom11">3</NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Trees Cycle" id="cycle-dropdown" className="cycle">
