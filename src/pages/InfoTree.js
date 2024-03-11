345c048c9d542e77e87df8ab42ffa5ad1ef142b5
Nikoru14 <nicholosantosbongat@gmail.com>
2024-03-12 00:15:59 +0800

    modified:   src/pages/InfoTree.js

--- src/pages/InfoTree.js
+++ src/pages/InfoTree.js
@@ -1,206 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import treeData from '../json/TreeData.json';
 import { useSearchParams } from 'react-router-dom';
-import { Container, Row, Col, Table, Button, Card } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import '../styles/infotree.css';
 
 const InfoTree = () => {
   const [searchParams] = useSearchParams();
   const treeId = searchParams.get('treeId');
   const [treeInfo, setTreeInfo] = useState(null);
-  const [itemActive, setItemActive] = useState(0);
-  const thumbnails = Array.from(document.querySelectorAll('.thumbnail .item'));
-  const items = document.querySelectorAll('.slider .list .item');
-  const countItem = items.length;
-
-  const showSlider = (index) => {
-    const itemActiveOld = document.querySelector('.slider .list .item.active');
-    const thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
-    itemActiveOld.classList.remove('active');
-    thumbnailActiveOld.classList.remove('active');
-
-    setItemActive(index);
-
-    const itemActiveNew = document.querySelector(`.slider .list .item:nth-child(${index + 1})`);
-    const thumbnailActiveNew = document.querySelector(`.thumbnail .item:nth-child(${index + 1})`);
-    itemActiveNew.classList.add('active');
-    thumbnailActiveNew.classList.add('active');
-  };
-
-  const handleNext = () => {
-    setItemActive((prevIndex) => (prevIndex + 1) % countItem);
-  };
-
-  const handlePrev = () => {
-    setItemActive((prevIndex) => (prevIndex - 1 + countItem) % countItem);
-  };
-
-  const startAutoSlide = () => {
-    const intervalId = setInterval(() => {
-      handleNext();
-    }, 5000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  };
-
-  
-  useEffect(() => {
-    startAutoSlide();
-  }, []);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
 
   useEffect(() => {
-    console.log('Retrieved treeId: ', treeId);
-
     if (treeId && treeData[treeId]) {
-      const info = treeData[treeId];
-      setTreeInfo(info);
+      setTreeInfo(treeData[treeId]);
     } else {
       console.log('treeId is null or not found in treeData');
     }
   }, [treeId]);
 
   useEffect(() => {
-    if (treeInfo) {
-      console.log('Tree Info: ', JSON.stringify(treeInfo, null, 2));
-    } else {
-      console.log('Tree Info is not available yet or treeId does not exist.');
-    }
+    const intervalId = setInterval(() => {
+      setActiveSlideIndex(prevIndex => (prevIndex + 1) % treeInfo?.imgUrl.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
   }, [treeInfo]);
+
+  const handleNext = () => {
+    setActiveSlideIndex(prevIndex => (prevIndex + 1) % treeInfo.imgUrl.length);
+  };
+
+  const handlePrev = () => {
+    setActiveSlideIndex(prevIndex => (prevIndex - 1 + treeInfo.imgUrl.length) % treeInfo.imgUrl.length);
+  };
+
   if (!treeInfo) {
-    return <div>Loading..</div>;
+    return <Container>Loading...</Container>;
   }
 
   return (
-    <div className="slider">
-              <div class="list">
-            <div class="item active">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image" />
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 01</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md={8}>
+          <div className="slider">
+            <img src={treeInfo.imgUrl[activeSlideIndex]} alt="Tree Image" className="slider-image" />
+            <div className="content">
+              <h2>{`Slider ${activeSlideIndex + 1}`}</h2>
+              <p>{treeInfo.description}</p>
             </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[0]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 02</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
+            <div className="arrows">
+              <Button variant="secondary" onClick={handlePrev}>{'<'}</Button>
+              <Button variant="secondary" onClick={handleNext}>{'>'}</Button>
             </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 03</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 04</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 05</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 06</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 07</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 08</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 09</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-            <img src={treeInfo.imgUrl[1]} alt="Tree Image"/>
-                <div class="content">
-                    <p>design</p>
-                    <h2>Slider 10</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div className="arrows">
-        <button onClick={handlePrev}>{'<'}</button>
-        <button onClick={handleNext}>{'>'}</button>
-      </div>
-      <div className="thumbnail">
-        {thumbnails.map((thumbnail, index) => (
-          <div key={index} className={`item ${index === itemActive ? 'active' : ''}`}>
-            <img src="./bg.png" alt={`Thumbnail ${index + 1}`} />
-            <div className="content">{`Name Slider ${index + 1}`}</div>
           </div>
-        ))}
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
-
 
 export default InfoTree;
