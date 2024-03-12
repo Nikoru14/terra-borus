37e659e20a573b014266580a5f65b3c959f955eb
Nikoru14 <nicholosantosbongat@gmail.com>
2024-03-13 00:50:10 +0800

    modified:   src/App.js 	modified:   src/components/NavigationBar.js 	deleted:    src/pages/AboutTree.js 	new file:   src/pages/AboutUs.js     deleted:    src/styles/abouttree.css 	new file:   src/styles/aboutus.css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ContactForm from './pages/ContactForm';
 import AdminSmspin from './pages/AdminSmspin';
 import InfoTree from './pages/InfoTree';
 import AddTree from './pages/AddTree';
-import AboutTree from './pages/AboutTree';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
@@ -29,7 +29,7 @@ function App() {
         <Route path='/AdminSmspin' element={<AdminSmspin />} />
         <Route path='/InfoTree' element={<InfoTree />} />
         <Route path='/AddTree' element={<AddTree />} />
-        <Route path='/AboutTree' element={<AboutTree />} />
+        <Route path='/AboutUs' element={<AboutUs />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
@@ -40,88 +40,88 @@ export default App;
 
 
 var w = window.innerWidth,
-    h = window.innerHeight,
-    canvas = document.getElementById('test'),
-    ctx = canvas.getContext('2d'),
-    rate = 60,
-    arc = 80,
-    time,
-    count,
-    size = 15,
-    speed = 40,
-    parts = new Array,
-    colors = ['green','forest','parakeet','lime','chartreuse'];
+  h = window.innerHeight,
+  canvas = document.getElementById('test'),
+  ctx = canvas.getContext('2d'),
+  rate = 60,
+  arc = 80,
+  time,
+  count,
+  size = 15,
+  speed = 40,
+  parts = new Array,
+  colors = ['green', 'forest', 'parakeet', 'lime', 'chartreuse'];
 var mouse = { x: 0, y: 0 };
 
-canvas.setAttribute('width',w);
-canvas.setAttribute('height',h);
+canvas.setAttribute('width', w);
+canvas.setAttribute('height', h);
 
 function create() {
   time = 0;
   count = 0;
 
-  for(var i = 0; i < arc; i++) {
+  for (var i = 0; i < arc; i++) {
     parts[i] = {
       x: Math.ceil(Math.random() * w),
       y: Math.ceil(Math.random() * h),
       toX: Math.random() * 5 - 1,
       toY: Math.random() * 2 - 1,
-      c: colors[Math.floor(Math.random()*colors.length)],
+      c: colors[Math.floor(Math.random() * colors.length)],
       size: Math.random() * size
     }
   }
 }
 
 function particles() {
-  ctx.clearRect(0,0,w,h);
-   canvas.addEventListener('mousemove', MouseMove, false);
-  for(var i = 0; i < arc; i++) {
+  ctx.clearRect(0, 0, w, h);
+  canvas.addEventListener('mousemove', MouseMove, false);
+  for (var i = 0; i < arc; i++) {
     var li = parts[i];
-    var distanceFactor = DistanceBetween( mouse, parts[i] );
-    var distanceFactor = Math.max( Math.min( 15 - ( distanceFactor / 10 ), 10 ), 1 );
+    var distanceFactor = DistanceBetween(mouse, parts[i]);
+    var distanceFactor = Math.max(Math.min(15 - (distanceFactor / 10), 10), 1);
     ctx.beginPath();
-    ctx.arc(li.x,li.y,li.size*distanceFactor,0,Math.PI*2,false);
+    ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
     ctx.fillStyle = li.c;
-    ctx.strokeStyle=li.c;
-    if(i%2==0)
+    ctx.strokeStyle = li.c;
+    if (i % 2 == 0)
       ctx.stroke();
     else
       ctx.fill();
-    
+
     li.x = li.x + li.toX * (time * 0.05);
     li.y = li.y + li.toY * (time * 0.05);
-    
-    if(li.x > w){
-       li.x = 0; 
+
+    if (li.x > w) {
+      li.x = 0;
     }
-    if(li.y > h) {
-       li.y = 0; 
+    if (li.y > h) {
+      li.y = 0;
     }
-    if(li.x < 0) {
-       li.x = w; 
+    if (li.x < 0) {
+      li.x = w;
     }
-    if(li.y < 0) {
-       li.y = h; 
+    if (li.y < 0) {
+      li.y = h;
     }
-   
-     
+
+
   }
-  if(time < speed) {
+  if (time < speed) {
     time++;
   }
-  setTimeout(particles,1000/rate);
+  setTimeout(particles, 1000 / rate);
 }
 function MouseMove(e) {
-   mouse.x = e.layerX;
-   mouse.y = e.layerY;
+  mouse.x = e.layerX;
+  mouse.y = e.layerY;
 
-   //context.fillRect(e.layerX, e.layerY, 5, 5);
-   //Draw( e.layerX, e.layerY );
+  //context.fillRect(e.layerX, e.layerY, 5, 5);
+  //Draw( e.layerX, e.layerY );
 }
-function DistanceBetween(p1,p2) {
-   var dx = p2.x-p1.x;
-   var dy = p2.y-p1.y;
-   return Math.sqrt(dx*dx + dy*dy);
+function DistanceBetween(p1, p2) {
+  var dx = p2.x - p1.x;
+  var dy = p2.y - p1.y;
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
 
