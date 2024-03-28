df795166b306c8647363ce3ce6b0b2dce2ab018d
symonchristopher-bolinas <angelessymon@gmail.com>
2024-03-28 23:57:35 +0800

    modified:   src/pages/AddTree.js 	renamed:    src/styles/addtree.css -> src/styles/addtree.module.css

--- src/pages/AddTree.js
+++ src/pages/AddTree.js
@@ -1,57 +1,65 @@
 import React, { useState } from 'react';
 import NavigationBar from "../components/NavigationBar";
-import '../styles/addtree.css';
+import addTreeCss from '../styles/addtree.module.css';
 
 class AddTree extends React.Component {
+  
   render() {
     return (
         <>
         <NavigationBar />
-      <div className="container">
-          <div className="card3">
-            <h1 className='addtree'>Add Tree</h1>
+      <div className={addTreeCss.container}>
+          <div className={addTreeCss.card3}>
+            <h1 className={addTreeCss.addtree}>Add Tree</h1>
             <form>
-            <div class="form-row">
-                    <div class="form-group col-md-6">
+            <div className={addTreeCss['form-row']}>
+                    <div className="col-md-6">
+                    <div className={addTreeCss.formGroup}>
                         <label for="inputSpecies">Choose Species</label>
-                        <select id="inputSpecies" class="form-control">
+                        <select id="inputSpecies" className="form-control">
                           <option selected>Choose...</option>
                           <option>...</option>
                         </select>
-                      </div>
-                      <div class="form-group col-md-6">
+                      </div></div>
+                      <div className="col-md-6">
+                      <div className={addTreeCss.formGroup}>
                         <label for="inputStatus">Choose Status</label>
-                        <select id="inputStatus" class="form-control">
+                        <select id="inputStatus" className={addTreeCss['form-control']} >
                           <option selected>Choose...</option>
                           <option>...</option>
                         </select>
-                      </div>
-                        <div class="form-group col-md-6">
+                      </div></div>
+                        <div className="col-md-6">
+                        <div className={addTreeCss.formGroup}>
                             <label for="inputTreeName">Name of Tree</label>
-                            <select id="inputTreeName" class="form-control">
+                            <select id="inputTreeName" className={addTreeCss['form-control']} >
                               <option selected>Choose...</option>
                               <option>...</option>
                             </select>
-                          </div>
-                          <div class="form-group col-md-6">
+                          </div></div>
+                          <div className="col-md-6">
+                          <div className={addTreeCss.formGroup}>
                             <label for="inputPlanterName">Name of Planter</label>
-                            <input type="name" class="form-control" id="name" aria-describedby="name" placeholder="Name"></input>
-                          </div>
-                         <div class="form-group col-md-4">
+                            <input type="name" className={addTreeCss['form-control']} id="name" aria-describedby="name" placeholder="Name"></input>
+                          </div></div>
+                         <div className="col-md-4">
+                         <div className={addTreeCss.formGroup}>
                             <label for="inputlongitude">Longitude</label>
-                            <input class="form-control" id="longitude" rows="1"></input>
-                         </div>
-                         <div class="form-group col-md-4">
+                            <input className={addTreeCss['form-control']}  id="longitude" rows="1"></input>
+                         </div></div>
+                         <div className="col-md-4">
+                         <div className={addTreeCss.formGroup}>
                             <label for="inputlatitude">Latitude</label>
-                            <input class="form-control" id="latitude" rows="1"></input>
-                         </div>
-                         <div class="form-group col-md-4">
-                                <label class="control-label" for="date">Date</label>
-                                <input type="datetime-local" class="form-control" id="date" name="date" placeholder="MM/DD/YYY"/>
-                         </div>
+                            <input className={addTreeCss['form-control']}  id="latitude" rows="1"></input>
+                         </div></div>
+                         <div className="col-md-4">
+                         <div className={addTreeCss.formGroup}>
+                                <label className="control-label" for="date">Date</label>
+                                <input type="datetime-local" className={addTreeCss['form-control']} id="date" name="date" placeholder="MM/DD/YYY"/>
+                         </div></div>
                           </div>
-              <div className="d-flex justify-content-center">
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <div className="d-flex justify-content-center">
+                <button type="submit" className={addTreeCss.btn}>Submit</button>
               </div>
             </form>
           </div>
